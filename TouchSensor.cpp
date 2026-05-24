cd16cda3dda6d14f9763cd763233b6e3009b32fb
Jan Kieseler <jkiesele@cern.ch>
2026-05-24 09:05:39 +0200

    touch sensor reference added

--- TouchSensor.cpp
+++ TouchSensor.cpp
@@ -2,13 +2,20 @@
 #include "driver/touch_pad.h"
 #include "threadSafeArduino.h"
 
-TouchSensor::TouchSensor(uint8_t pin, uint16_t threshold, uint16_t hysteresis,  uint8_t samples, uint8_t nMovingAvg)
-    : pin_(pin), threshold_(threshold), hysteresis_(hysteresis), state_(false), lastValue_(0), samples_(samples), nMovingAvg_(nMovingAvg)
+TouchSensor::TouchSensor(uint8_t pin, uint16_t threshold, uint16_t hysteresis,  uint8_t samples, uint8_t nMovingAvg,
+int referencePin)
+    : pin_(pin), threshold_(threshold), hysteresis_(hysteresis), state_(false), 
+    lastValue_(0), samples_(samples), nMovingAvg_(nMovingAvg), referencePin_(referencePin)
 {
 }
 
 void TouchSensor::update() {
     auto thisvalue = threadSafe::touchRead(pin_);
+    if(referencePin_ >= 0) {
+        uint32_t referenceValue = threadSafe::touchRead(referencePin_);
+        // add a simple fixed offset to keep things uint16_t and avoid underflow
+        thisvalue = ((uint32_t)thisvalue + 10000) - referenceValue;
+    }
     if(thisvalue == lastValue_)
         return; // no change => do nothing we might be calling over sampling frequency
     
--- TouchSensor.h
+++ TouchSensor.h
@@ -3,7 +3,8 @@
 
 class TouchSensor {
 public:
-    TouchSensor(uint8_t pin, uint16_t threshold, uint16_t hysteresis = 200, uint8_t samples = 3, uint8_t nMovingAvg = 0);
+    TouchSensor(uint8_t pin, uint16_t threshold, uint16_t hysteresis = 200, uint8_t samples = 3, uint8_t nMovingAvg = 0,
+    int referencePin=-1);
 
     void update();  // Call this in loop()
     bool isActive() const;
@@ -28,4 +29,5 @@ private:
     uint8_t samples_;
     uint8_t sampleCount = 0;
     uint8_t nMovingAvg_;
+    int referencePin_;
 };
